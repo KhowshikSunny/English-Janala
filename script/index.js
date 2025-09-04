de2cb3903a9bb2eb7125c9c954162b14ea7b9c9f
Khowshik Sunny <khowshik.sunny7@gmail.com>
2025-09-04 09:31:02 +0600

    resonsive and pronunciation function added

--- script/index.js
+++ script/index.js
@@ -1,3 +1,8 @@
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
 const createElements = (arr) => {
   const htmlElements = arr.map((el) => `<span class="btn"> ${el}</span>`);
   return htmlElements.join(" ");
@@ -102,13 +107,6 @@ const displayLevelWord = (words) => {
   words.forEach((word) => {
     // word card
 
-    // {
-    // "id": 82,
-    // "level": 1,
-    // "word": "Car",
-    // "meaning": "গাড়ি",
-    // "pronunciation": "কার"
-    // }
 
     // console.log(word)
     const card = document.createElement("div");
@@ -127,7 +125,7 @@ const displayLevelWord = (words) => {
           <button onclick="loadWordDetail(${
             word.id
           })" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] color-[#374957]"> <i class="fa-solid fa-circle-info"></i></button>
-          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] color-[#374957]"> <i class="fa-solid fa-volume-high"></i></button>
+          <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] color-[#374957]"> <i class="fa-solid fa-volume-high"></i></button>
         </div>
 
       </div>
