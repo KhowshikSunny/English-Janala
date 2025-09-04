058a96c2d697f4a83696fb3ebc3ee3d7771af3d0
Khowshik Sunny <khowshik.sunny7@gmail.com>
2025-09-04 06:52:56 +0600

    modal tamplate design done without dynamic

--- script/index.js
+++ script/index.js
@@ -3,29 +3,114 @@ const loadLessons = () => {
     .then((response) => response.json())
     .then((json) => displayLesson(json.data));
 };
+
+const removeActive = () => {
+  const lessonButtonAll = document.querySelectorAll(".lesson-btn");
+  lessonButtonAll.forEach((btn) => btn.classList.remove("active"));
+};
 const loadLevelWord = (id) => {
   // console.log(id)
 
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((json) => displayLevelWord(json.data));
+    .then((json) => {
+      removeActive();
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+      clickBtn.classList.add("active");
+      displayLevelWord(json.data);
+    });
 };
+
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  const res = await fetch(url)
+  const details =await res.json()
+  displayWordDetails(details.data)
+
+};
+
+// {
+//     "word": "Cautious",
+//     "meaning": "সতর্ক",
+//     "pronunciation": "কশাস",
+//     "level": 2,
+//     "sentence": "Be cautious while crossing the road.",
+//     "points": 2,
+//     "partsOfSpeech": "adjective",
+//     "synonyms": [
+//         "careful",
+//         "alert",
+//         "watchful"
+//     ],
+//     "id": 3
+// }
+const displayWordDetails=(word)=>{
+    console.log(word)
+    const detailsBox = document.getElementById("details-container")
+    detailsBox.innerHTML=`
+    <div class="">
+        <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation})</h2>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Meaning</h2>
+        <p>${word.meaning}</p>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Example</h2>
+        <p>${word.sentence}</p>
+      </div>
+      <div class="">
+        <h2 class="font-bold">synoname</h2>
+        <span class="btn">syn</span>
+        <span class="btn">syn</span>
+        <span class="btn">syn</span>
+      </div>
+    `
+    document.getElementById("word_modal").showModal()
+
+}
 
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
+
+  if (words.length == 0) {
+    wordContainer.innerHTML = `<div class="text-center  col-span-full rounded-xl py-10 space-y-6 font-bangla">
+    <img class="mx-auto" src="./assets/alert-error.png" alt="">
+        <p class="text-xl  font-light text-gray-500" >এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-2xl">নেক্সট Lesson এ যান</h2>
+      </div>`;
+    return;
+  }
   words.forEach((word) => {
     // word card
+
+    // {
+    // "id": 82,
+    // "level": 1,
+    // "word": "Car",
+    // "meaning": "গাড়ি",
+    // "pronunciation": "কার"
+    // }
+
     // console.log(word)
     const card = document.createElement("div");
     card.innerHTML = `
-        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-        <h2 class="font-bold text-2xl">${word.word}</h2>
+        <div class="bg-white rounded-xl shadow-sm text-center h-[100%] py-10 px-5 space-y-4">
+        <h2 class="font-bold text-2xl">${
+          word.word ? word.word : "শব্দ পাওয়া যায়নি"
+        }</h2>
         <p class="font-semibold ">Meaning /Pronounciation</p>
-        <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation} "</div>
+        <div class="text-2xl font-medium font-bangla">"${
+          word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"
+        } / ${
+      word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি "
+    } "</div>
         <div class="flex justify-between items-center">
-          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] color-[#374957]"> <i class="fa-solid fa-circle-info"></i></button>
+          <button onclick="loadWordDetail(${
+            word.id
+          })" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] color-[#374957]"> <i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] color-[#374957]"> <i class="fa-solid fa-volume-high"></i></button>
         </div>
 
@@ -46,7 +131,7 @@ const displayLesson = (lessons) => {
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
     
-              <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"
+              <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"
                 ><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button
               >
             
@@ -55,4 +140,5 @@ const displayLesson = (lessons) => {
     levelContainer.append(btnDiv);
   }
 };
+
 loadLessons();
